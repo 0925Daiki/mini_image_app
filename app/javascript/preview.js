fc596084335b1e8d05910bcd58d7ff6d4cf94c89
endodaiki <82555115+0925Daiki@users.noreply.github.com>
2021-07-07 13:30:33 +0900

    複数画像投稿

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -6,21 +6,35 @@ document.addEventListener('DOMContentLoaded', function(){
     const createImageHTML = (blob) => {
       //画像を表示するためのdiv要素を生成
       const imageElement = document.createElement('div');
+      imageElement.setAttribute('class', "image-element");
+      let imageElementNum = document.querySelectorAll('.image-element').length;
+
       //表示する画像を生成
       const blobImage = document.createElement('img');
       blobImage.setAttribute('src', blob);
 
+      //ファイルを選択するボタンを生成
+      const inputHTML = document.createElement('input');
+      //変数imageElementNumを定義することで、先ほど作成したinput要素が「何番目の要素か」を判別
+      inputHTML.setAttribute('id', `message_image_${imageElementNum}`);
+      inputHTML.setAttribute('name', 'message[images][]');
+      inputHTML.setAttribute('type', 'file');
+
       //生成したHTMLの要素をブラウザに表示させる
       imageElement.appendChild(blobImage);
+      imageElement.appendChild(inputHTML);
       ImageList.appendChild(imageElement);
+
+      inputHTML.addEventListener('change',(e) => {
+        file = e.target.files[0];
+        blob = window.URL.createObjectURL(file);
+
+        createImageHTML(blob);
+      });
     };
-    
+
     document.getElementById('message_image').addEventListener('change',function(e){
-      //画像が表示さている場合のみ、すでに存在している画像を削除する
-      const imageContent = document.querySelector('img');
-      if (imageContent){
-        imageContent.remove();
-      }
+
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
 
